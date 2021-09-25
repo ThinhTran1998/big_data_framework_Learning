9a36ef9d9521d6a71e6ca60e1d78b828485a9618
ThinhTran1998 <tranxquocthinh@gmail.com>
2021-09-25 23:18:06 +0700

    'update'

---
+++ kafka_to_spark.py
@@ -0,0 +1,33 @@
+# Imports and running findspark
+import findspark
+
+findspark.init()
+
+import os
+os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.0.2 pyspark-shell'
+
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
+
+# Spark context details
+sc = SparkContext(appName="PythonSparkStreamingKafka")
+ssc = StreamingContext(sc, 2)
+# Creating Kafka direct stream
+dks = KafkaUtils.createDirectStream(ssc, topics=['test_Thinh'],
+                                    kafkaParams={"metadata.broker.list": "localhost:9092"})
+
+# print(type(dks))
+
+lines = dks.map(lambda x: x[1])
+counts = lines.flatMap(lambda line: line.split(" "))\
+    .map(lambda word: (word, 1))\
+    .reduceByKey(lambda a, b: a + b)
+counts.pprint()
+
+# counts = dks.pprint()
+
+ssc.start()
+ssc.awaitTermination()
+
+# spark-submit --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.3.0 kafka_to_spark.py
